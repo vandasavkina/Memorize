f461e8e05f3e3b1c3dbc6b3eb8495264f00f1460
vandasavkina <wandasavkina@hotmail.it>
2024-04-20 23:24:02 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Vanda Savkina on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
